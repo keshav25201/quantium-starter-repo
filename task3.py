9071977d13ce6b88dacff6715855add8d04b2bcc
keshav25201 <keshav25201@gmail.com>
2022-09-24 13:03:32 +0530

    task3

--- task3.py
+++ task3.py
@@ -5,14 +5,15 @@ import pandas as pd
 app = Dash(__name__)
 df = pd.read_csv('output.csv')
 data = df.groupby('date',as_index=False).sum()
-fig = px.bar(data, x="date", y="sales",title="sales per day")
+fig = px.line(data, x="date", y="sales",title="sales per day")
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
-
+    html.H1('PINK MORSELS SALES ANALYSIS'),
+    dcc.RadioItems(
+                ['north','east','south','west','all'],
+                'all',
+                id='xaxis-type',
+                inline=True
+            ),
     dcc.Graph(
         id='example-graph',
         figure=fig
