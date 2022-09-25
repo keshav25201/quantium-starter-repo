ccf88d077b835f8ef639de2b5ccaf9c5b6a47526
keshav25201 <keshav25201@gmail.com>
2022-09-25 22:20:54 +0530

    testing

--- test_.py
+++ test_.py
@@ -3,4 +3,8 @@ from task3 import app
 def test_header001(dash_duo):
     dash_duo.start_server(app)
     dash_duo.wait_for_element("#region", timeout=10)
+def test_graph002(dash_duo):
+    dash_duo.start_server(app)
+    dash_duo.wait_for_element("#example-graph", timeout=10)
+
 
